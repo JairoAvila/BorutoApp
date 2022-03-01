b2cf88e69ae8e38cdfd8e8885f9db3c31d3c407c
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-02-28 19:23:46 -0500

    Top bar is created

--- app/src/main/java/com/jairoavila/borutoapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/jairoavila/borutoapp/presentation/home/HomeScreen.kt
@@ -1,8 +1,11 @@
 package com.jairoavila.borutoapp.presentation.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
 
+    Scaffold(topBar = { HomeTopBar {} }) {
+    }
 }
