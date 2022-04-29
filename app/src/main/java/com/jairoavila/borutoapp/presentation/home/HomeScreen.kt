1abae529ac6cf96310100797971a44cb857f2322
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-04-28 21:01:34 -0500

    Design RatingWidget

--- app/src/main/java/com/jairoavila/borutoapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/jairoavila/borutoapp/presentation/home/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.jairoavila.borutoapp.presentation.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
+import com.jairoavila.borutoapp.presentation.components.RatingWidget
+import com.jairoavila.borutoapp.ui.theme.LARGE_PADDING
 
 @ExperimentalCoilApi
 @Composable
@@ -18,5 +22,10 @@ fun HomeScreen(
         topBar = {
             HomeTopBar(onSearchClicked = {})
         }
-    ) {}
+    ) {
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING),
+            rating = 0.5
+        )
+    }
 }
